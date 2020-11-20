3131d9a82e19fc676a7c09bd56ab69957fb12d07
Crhistina <crhistina.javier.15@gmail.com>
2020-11-19 19:46:06 -0500

    Inserta Libro

---
+++ src/main/java/com/proyecto/ExamenPc023Application.java
@@ -0,0 +1,13 @@
+package com.proyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenPc023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenPc023Application.class, args);
+	}
+
+}
